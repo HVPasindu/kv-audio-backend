168e73a51c89f334030211e250b3c8ede8b2da18
HVPasindu <hvpmjayarathna2001@gmail.com>
2025-05-04 02:52:45 +0530

    mongdb is connected 🥰

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello");
+import express from "express"
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(bodyParser.json());
+
+const mongoUrl = "mongodb+srv://admin:123@cluster0.pwerylk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoUrl);
+let connection = mongoose.connection
+connection.once("open",()=>{
+    console.log("Mongodb connection successfully...")
+})
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("sever is running....")
+})
