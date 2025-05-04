2c830cb6e27e1c47f796d5dd66efb3964338afb7
HVPasindu <hvpmjayarathna2001@gmail.com>
2025-05-04 10:19:28 +0530

    add some data into the monogodb 😊

--- index.js
+++ index.js
@@ -12,7 +12,43 @@ connection.once("open",()=>{
     console.log("Mongodb connection successfully...")
 })
 
+app.post("/",(req,res)=>{
+    const userSchema = mongoose.Schema({
+        name:String,
+        number:String,
+        age:Number
+    })
+    let User = mongoose.model("model",userSchema);
+    let newUser = req.body;
+    let user =new User(newUser);
 
+    user.save().then(()=>{
+        res.json({
+            message:"user saved success",
+        })
+    }).catch((error)=>{
+        res.status(401).json({
+            error:"user not add in database...."
+        })
+    })
+
+})
+
+app.get("/",(req,res)=>{
+    const userSchema = mongoose.Schema({
+        name:String,
+        number:String,
+        age:Number
+    })
+    let User = mongoose.model("model",userSchema);
+    User.find().then((result)=>{
+        res.json(result)
+    }).catch(()=>{
+        res.json({
+            message:"error occured..."
+        })
+    })
+})
 
 
 
