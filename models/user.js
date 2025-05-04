f28275587c0193536d1016eabb716dccd9b2a909
HVPasindu <hvpmjayarathna2001@gmail.com>
2025-05-04 11:21:36 +0530

    add some users to database

--- models/user.js
+++ models/user.js
@@ -1,10 +1,33 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    name:String,
-    number:String,
-    age:Number
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true
+    },
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true,
+        default:"customer"
+    }
 })
-let User = mongoose.model("model",userSchema);
+let User = mongoose.model("user",userSchema);
 
 export default User;
