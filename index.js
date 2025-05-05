686473e7776a4f8865d042ef8675acf3d15aa066
HVPasindu <hvpmjayarathna2001@gmail.com>
2025-05-06 01:48:18 +0530

    token is replace

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(bodyParser.json());
+app.use((req,res,next)=>{
+    let token = req.header("Authorization");
+    console.log(token)
+    
+    if(token!=null){
+        token=token.replace("Bearer ","")
+        jwt.verify(token,"kv secret 89",(err,decoded)=>{
+            if(!err){
+                req.user=decoded;
+                
+            }
+        })
+    }
+    next();
+})
 
 const mongoUrl = "mongodb+srv://admin:123@cluster0.pwerylk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoUrl);
