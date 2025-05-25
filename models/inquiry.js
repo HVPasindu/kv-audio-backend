07696ceaed4d032f69bf74dd398f8c7b95b56ad6
HVPasindu <hvpmjayarathna2001@gmail.com>
2025-05-25 23:48:33 +0530

    create inquiry model 😊

---
+++ models/inquiry.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const inquirySchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    Phone:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:date,
+        required:true,
+        default:Date.now()
+    },
+    id:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    response:{
+        type:String,
+        required:true,
+        default:""
+    },
+    issolved:{
+        type:Boolean,
+        required:true,
+        default:false
+    }
+
+})
+
+const Inquiry = mongoose.model("Inquiry",inquirySchema);
+
+export default Inquiry;
